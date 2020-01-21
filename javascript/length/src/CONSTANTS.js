fc22fee0d5c7f35ff37df0eef2404401802f8d82
stonelv <zhe.lv.82@gmail.com>
2020-01-21 10:05:59 +0800

    fit an error in CONSTANTS.js

--- javascript/length/src/CONSTANTS.js
+++ javascript/length/src/CONSTANTS.js
@@ -1,9 +1,8 @@
-export default class CONSTANTS {
-  yard_to_foot = 3
-  yard_to_inch = 36
-  foot_to_inch = 12
-
-  YARD = 'yard'
-  INCH = 'inch'
-  FOOT = 'foot'
+export const CONSTANTS = {
+  yard_to_foot: 3,
+  yard_to_inch: 36,
+  foot_to_inch: 12,
+  YARD: 'yard',
+  INCH: 'inch',
+  FOOT: 'foot',
 }
